3c4c29baec673621b1eb304c785733c127c89eeb
Emily vandenheever <Emvdh@Emilys-MacBook-Air.local>
2021-07-28 13:59:20 -0500

    fix contact form email color

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -55,7 +55,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a className="text-green-400 leading-relaxed">
                 benstof@email.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
