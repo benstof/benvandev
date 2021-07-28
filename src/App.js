6aa234d68c16c53043c538c4df02bc083c1e59a0
Emily vandenheever <Emvdh@Emilys-Air.attlocal.net>
2021-07-27 22:11:36 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      
+    </main>
+  )
+}
